673430771bc941e429b6709c2d1f021f3d8611ce
MorriganH <morriganhennessy@gmail.com>
2023-05-17 14:48:36 -0400

    sets up PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,24 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import '../styles/PhotoListItem.scss';
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const { username, imageSource, id, hideUserName, likedByUser } = props;
+
+  const [ like, setLike ] = useState(likedByUser);
+
+  const toggleLike = () => setLike(like ? false : true);
+
+  return (
+    <article className='photo-list--item'>
+      <img src={imageSource} className='photo-list--image'></img>
+      <div className='photo-list--user-profile'>
+        {hideUserName ? '' : <span>{username}</span>}
+      </div>
+      <button onClick={toggleLike}>{like ? '❤️' : '🤍' }</button>
+    </article>
+  )
 }
 
 PhotoListItem.defaultProps = {
@@ -12,6 +26,7 @@ PhotoListItem.defaultProps = {
   imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
   id: 1,
   hideUserName: false,
+  likedByUser: false
 }
 
 export default PhotoListItem
