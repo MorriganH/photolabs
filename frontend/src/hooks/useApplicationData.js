14397b8b084a317a368d81698fcc1ec33e57dc8f
MorriganH <morriganhennessy@gmail.com>
2023-05-27 19:55:31 -0400

    prepares for useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,23 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 const useApplicationData = () => {
-  const [modalShown, setModalShown] = useState(false);
-  const [photoData, setPhotoData] = useState();
-  const [likedPhotos, setLikedPhoto] = useState({});
-  const [photos, setPhotos] = useState([]);
-  const [topics, setTopics] = useState([]);
-  const [filteredPhotos, setFilteredPhotos] = useState([]);
-  const [filterID, setFilterID] = useState("");
+  // const [modalShown, setModalShown] = useState(false);
+  // const [photoData, setPhotoData] = useState();
+  // const [likedPhotos, setLikedPhoto] = useState({});
+  // const [photos, setPhotos] = useState([]);
+  // const [topics, setTopics] = useState([]);
+  // const [filteredPhotos, setFilteredPhotos] = useState([]);
+  // const [filterID, setFilterID] = useState("");
 
-  const state = {
-    modalShown,
-    photoData,
-    likedPhotos,
-    photos,
-    topics,
-    filteredPhotos,
-    filterID,
+  const initialState = {
+    photos: [],
+    topics: [],
+    likedPhotos: {},
+    modalShown: false,
+    photoData: undefined,
+    filteredPhotos: [],
+    filterID: "",
   };
+  const [state, setState] = useState(initialState);
+
+  // const state = {
+  //   modalShown,
+  //   photoData,
+  //   likedPhotos,
+  //   photos,
+  //   topics,
+  //   filteredPhotos,
+  //   filterID,
+  // };
 
   // fetches photos and topics from the database, only runs once after first load
   useEffect(() => {
@@ -28,7 +39,8 @@ const useApplicationData = () => {
         return response.json();
       })
       .then(json => {
-        setPhotos(json);
+        // setPhotos(json);
+        setState({ ...state, photos: json });
       })
       .catch(e => console.log(e));
 
@@ -39,14 +51,16 @@ const useApplicationData = () => {
         return response.json();
       })
       .then(json => {
-        setTopics(json);
+        // setTopics(json);
+        setState({ ...state, topics: json });
       })
       .catch(e => console.log(e));
   }, []);
 
   // gets the id of the category that the user clicked
   const filterPhotos = id => {
-    setFilterID(id);
+    // setFilterID(id);
+    setState({ ...state, filterID: id });
   };
 
   // fetches photos of a certain category from the database
@@ -57,7 +71,8 @@ const useApplicationData = () => {
           return response.json();
         })
         .then(json => {
-          setFilteredPhotos(json);
+          // setFilteredPhotos(json);
+          setState({ ...state, filteredPhotos: json });
         })
         .catch(e => console.log(e));
     }
@@ -65,21 +80,33 @@ const useApplicationData = () => {
 
   // toggles the modal to be displayed or removed when the user clicks an image, or the X icon in the modal
   function toggleShowModal(id) {
-    const photo = photos.find(photo => photo.id === id);
-    setModalShown(modalShown ? false : true);
-    setPhotoData(photo);
+    const photo = state.photos.find(photo => photo.id === id);
+    // setModalShown(state.modalShown ? false : true);
+    // setPhotoData(photo);
+    setState({
+      ...state,
+      modalShown: id ? true : false,
+      photoData: photo,
+    });
   }
 
   // adds/clears image id from the likedPhotos state
   function setLikedPhotos(id) {
-    setLikedPhoto(prev => {
-      const newLiked = {...prev};
-      if (newLiked[id] === true) {
-        newLiked[id] = null;
-      } else {
-        newLiked[id] = true;
-      }
-      return newLiked;
+    // setLikedPhoto(prev => {
+    //   const newLiked = { ...prev };
+    //   if (newLiked[id] === true) {
+    //     newLiked[id] = null;
+    //   } else {
+    //     newLiked[id] = true;
+    //   }
+    //   return newLiked;
+    // });
+    setState({
+      ...state,
+      likedPhotos: {
+        ...state.likedPhotos,
+        id: state.likedPhotos[id] ? null : true,
+      },
     });
   }
 
